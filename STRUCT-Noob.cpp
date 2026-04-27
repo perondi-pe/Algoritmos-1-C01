c0e40b7ae125b43f80e584a622e46c7ab335d5fc
Pedro Perondi Gadbem <gadbem.sfw@gmail.com>
2026-04-27 16:17:01 -0300

    Create STRUCT-Noob.cpp

---
+++ STRUCT-Noob.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct dados{
+    int matricula;
+    float nota1;
+    float nota2;
+};
+
+int main(){
+    dados aluno;
+    cin >> aluno.matricula >> aluno.nota1 >> aluno.nota2;
+
+    cout << "Matricula do aluno: " << aluno.matricula << endl;
+    cout << fixed << setprecision(2);
+    cout << "Nota da primeira prova: " << aluno.nota1 << endl;
+    cout << "Nota da segunda prova: " << aluno.nota2 << endl;
+    cout << "Media: " << ((aluno.nota1 * 2) + (aluno.nota2 * 3)) / 5 << endl;
+}
