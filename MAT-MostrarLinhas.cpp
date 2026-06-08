a1ac0064279d52b6db8c65fd9cd91a2ed757a82f
Pedro Perondi Gadbem <gadbem.sfw@gmail.com>
2026-06-08 10:56:54 -0300

    Create MAT-MostrarLinhas.cpp

---
+++ MAT-MostrarLinhas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    int mat[100][100];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> mat[i][j];
+        }
+        cin.ignore();
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
